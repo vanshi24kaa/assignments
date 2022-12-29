3052cd1e7f32fe19a4ada1b4c739741d12ad15ed
vanshi24kaa <116299114+vanshi24kaa@users.noreply.github.com>
2022-12-30 01:06:13 +0530

    Add files via upload

---
+++ Lambda7.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+    public class Lambda7{
+        public static void main(String[] args) {
+            HashMap<String,Integer> map = new HashMap<>();
+            map.put("a",4);
+            map.put("b",2);
+            map.put("c",3);
+            Lambda7 KeyValues = new Lambda7();
+            KeyValues.convertKeyValueToString(map);
+        }
+        public String convertKeyValueToString(HashMap<String, Integer> map) {
+
+            StringBuilder str = new StringBuilder();
+            Consumer<Map.Entry> consumer=(p)->str.append(p.getKey()).append(p.getValue());
+            Set set  = map.entrySet();
+            set.stream()
+                    .forEach(consumer);
+            System.out.println(str);
+            return str.toString();
+        }
+    }
+
